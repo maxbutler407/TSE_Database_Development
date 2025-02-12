53d2c936c3479ffc5d4b3714b9723a4abbe41495
ChengyuXue <161510954+ChengyuXue@users.noreply.github.com>
2025-02-12 22:27:05 +0000

    Update db.js

--- farm-api/db.js
+++ farm-api/db.js
@@ -1,13 +1,24 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST,  
+  user: process.env.MYSQL_USER,  
+  password: process.env.MYSQL_PASSWORD,  
+  database: process.env.MYSQL_DATABASE,  
+  port: process.env.MYSQL_PORT,  
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+});
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("MySQL Connection Failed:", err);
+  } else {
+    console.log("MySQL Connected Successfully!");
+    connection.release();
+  }
 });
 
 module.exports = pool.promise();
