34c06aabd8a54b47eb25dcbe67369272209821cb
ChengyuXue <161510954+ChengyuXue@users.noreply.github.com>
2025-03-30 11:10:59 +0100

    Update db.js

--- farm-api/db.js
+++ farm-api/db.js
@@ -1,3 +1,11 @@
+console.log("[ENV CHECK]", {
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
+});
+
 require("dotenv").config();
 const mysql = require("mysql2");
 
