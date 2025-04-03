d629af9d178ebc5474c351c8773110f870de5526
Max Butler <158061940+maxbutler407@users.noreply.github.com>
2025-04-03 16:52:54 +0100

    Update db.js

--- farm-api/db.js
+++ farm-api/db.js
@@ -9,17 +9,6 @@ console.log("[ENV CHECK]", {
 require("dotenv").config();
 const mysql = require("mysql2");
 
-import mysql from "mysql2";
-import dotenv from "dotenv";
-dotenv.config();
-
-console.log("🔍 Connecting to MySQL with:");
-console.log("Host:", process.env.MYSQLHOST);
-console.log("Port:", process.env.MYSQLPORT);
-console.log("User:", process.env.MYSQLUSER);
-console.log("Database:", process.env.MYSQLDATABASE);
-
-
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,  
   user: process.env.MYSQL_USER,  
